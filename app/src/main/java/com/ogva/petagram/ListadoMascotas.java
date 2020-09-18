eabc4115de38331f194b100d7abded0ae28fbb5a
Oswaldo Gerardo Velasquez Aroni <oswaldo.velasquez@gmail.com>
2020-09-18 10:35:03 -0400

    Se agrego dos Activities

---
+++ app/src/main/java/com/ogva/petagram/ListadoMascotas.java
@@ -0,0 +1,14 @@
+package com.ogva.petagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListadoMascotas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listado_mascotas);
+    }
+}
