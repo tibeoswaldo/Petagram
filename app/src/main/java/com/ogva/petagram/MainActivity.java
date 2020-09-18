605e3c5473de752f84018c101bb1c686264c54e9
Oswaldo Gerardo Velasquez Aroni <oswaldo.velasquez@gmail.com>
2020-09-18 18:17:53 -0400

    SnackBar

--- app/src/main/java/com/ogva/petagram/MainActivity.java
+++ app/src/main/java/com/ogva/petagram/MainActivity.java
@@ -3,6 +3,12 @@ package com.ogva.petagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        agregarFAB();
+    }
+
+    public void agregarFAB() {
+        //FloatingActionButton miFAB = (FloatingActionButton)findViewById(R.id.fabMiFAB);
+        FloatingActionButton miFAB = findViewById(R.id.fabMiFAB);
+        miFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getBaseContext(),getResources().getString(R.string.mensaje), Toast.LENGTH_SHORT).show();
+                //Snackbar.make(v, getResources().getString(R.string.mensaje),Snackbar.LENGTH_SHORT).show();
+                Snackbar.make(v, getResources().getString(R.string.mensaje),Snackbar.LENGTH_LONG)
+                        .setAction(getResources().getString(R.string.texto_accion), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Log.i("SNACKBAR", "Click en Snackbar");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.colorPrimary))
+                        .show();
+            }
+        });
     }
 }
