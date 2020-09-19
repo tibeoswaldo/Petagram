62cda4397dc4a935ed2981823d69f164a7b8626d
Oswaldo Gerardo Velasquez Aroni <oswaldo.velasquez@gmail.com>
2020-09-19 16:40:36 -0400

    Se agrego un CardView

--- app/src/main/java/com/ogva/petagram/MainActivity.java
+++ app/src/main/java/com/ogva/petagram/MainActivity.java
@@ -1,10 +1,14 @@
 package com.ogva.petagram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,11 +16,35 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    SwipeRefreshLayout sfiMiIndicadorRefresh;
+    ListView lstMiLista;
+    Adapter adaptador;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         agregarFAB();
+
+        sfiMiIndicadorRefresh = findViewById(R.id.sfiMiIndicadorRefresh);
+        lstMiLista = findViewById(R.id.lstMiLista);
+
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, planetas));
+
+        sfiMiIndicadorRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refrescandoContenido();
+            }
+        });
+
+    }
+
+    public void refrescandoContenido() {
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, planetas));
+        sfiMiIndicadorRefresh.setRefreshing(false);
     }
 
     public void agregarFAB() {
